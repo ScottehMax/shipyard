268903de017f26411673aac54d4f97445b4e2494
Tim Hull <t.hull1@outlook.com>
2016-02-20 14:57:01 +0000

    Added Boat model

--- syWeb/api/models/Boat.js
+++ syWeb/api/models/Boat.js
@@ -1,14 +1,28 @@
 /**
 * Boat.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
 */
 
 module.exports = {
 
-  attributes: {
+    attributes: {
+        name: {
+            type: 'string'
+        },
 
-  }
+        port: {
+            type: 'integer'
+        },
+
+        active: {
+            type: 'boolean'
+        },
+
+        gitUrl: {
+            type: 'string'
+        },
+
+        owner: {
+            model: 'user'
+        }
+    }
 };
-
