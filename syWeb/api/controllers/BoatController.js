2b6fa51bb595f18cf7a3615e28dc674c7d63f193
Nathan Douglas <nd9600@gmail.com>
2016-02-20 19:33:48 +0000

    Added async to bC

--- syWeb/api/controllers/BoatController.js
+++ syWeb/api/controllers/BoatController.js
@@ -5,120 +5,121 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+ var async = require('async');
+
  function getLastPullTime(boat, callback) {
 
- 	// find last pull time
- 	Log.findOne({where: { boat: boat.id, type: 'pull'}, sort: 'createdAt DESC'}).exec(function (err,log) {
- 		if(err){
- 			return res.json({
- 				error:err
- 			});
- 		}
+     // find last pull time
+     Log.findOne({where: { boat: boat.id, type: 'pull'}, sort: 'createdAt DESC'}).exec(function (err,log) {
+         if(err){
+             return res.json({
+                 error:err
+             });
+         }
 
- 		console.log(log);
+         console.log(log);
 
- 		if(log !== undefined) {
- 			callback(log.createdAt);
- 		} else {
-			callback(null);
-		}
+         if(log !== undefined) {
+             callback(log.createdAt);
+         } else {
+            callback(null);
+        }
 
- 	});
+     });
  }
 
  function getUptime(boat, callback) {
 
-	 // find and calculate uptime
-	 Log.findOne({where: { boat: boat.id, type: 'up'}, sort: 'createdAt DESC'}).exec(function (err,log) {
-		 if(err){
-			 return res.json({
-				 error:err
-			 });
-		 }
+     // find and calculate uptime
+     Log.findOne({where: { boat: boat.id, type: 'up'}, sort: 'createdAt DESC'}).exec(function (err,log) {
+         if(err){
+             return res.json({
+                 error:err
+             });
+         }
 
-		 if(log !== undefined) {
-			 var currentTime = Date.now();
-			 var upAt = Date.parse(log.createdAt);
-			 var uptime = Math.floor((currentTime - upAt)/1000);
-			 callback(uptime);
-		 } else {
-			 callback(null);
-		 }
+         if(log !== undefined) {
+             var currentTime = Date.now();
+             var upAt = Date.parse(log.createdAt);
+             var uptime = Math.floor((currentTime - upAt)/1000);
+             callback(uptime);
+         } else {
+             callback(null);
+         }
 
-	 });
+     });
 
  }
 
 module.exports = {
 
-	get: function (req, res) {
-		var boatID = req.param('id');
-		Boat.findOne({id:boatID}).exec(function (err,boat) {
-        	if(err){
-            	return res.json({
-              		error:err
-            	});
-          	}
+    get: function (req, res) {
+        var boatID = req.param('id');
+        Boat.findOne({id:boatID}).exec(function (err,boat) {
+            if(err){
+                return res.json({
+                      error:err
+                });
+              }
 
-          	if(boat === undefined) {
-            	return res.notFound();
-          	}
-          	else {
-				var boatObj = {};
-			  	boatObj.name = boat.name;
-			  	boatObj.port = boat.port;
-			  	boatObj.active = boat.active;
+              if(boat === undefined) {
+                return res.notFound();
+              }
+              else {
+                var boatObj = {};
+                  boatObj.name = boat.name;
+                  boatObj.port = boat.port;
+                  boatObj.active = boat.active;
 
-				boatObj.lastUpdated = getLastPullTime(boat);
-				boatObj.uptime = getUptime(boat);
+                boatObj.lastUpdated = getLastPullTime(boat);
+                boatObj.uptime = getUptime(boat);
 
-				return res.json(boatObj);
-			}
+                return res.json(boatObj);
+            }
 
-		});
+        });
 
 
-	},
+    },
 
-	getAll: function (req, res) {
+    getAll: function (req, res) {
 
-		var fleetOfBoats = [];
+        var fleetOfBoats = [];
 
-		Boat.find().exec(function (err,boats) {
-        	if(err){
-            	return res.json({
-              		error:err
-            	});
-          	}
+        Boat.find().exec(function (err,boats) {
+            if(err){
+                return res.json({
+                      error:err
+                });
+              }
 
-          	if(boats === undefined) {
-            	return res.notFound();
-			} else {
+              if(boats === undefined) {
+                return res.notFound();
+            } else {
 
-		        for (var i = 0; i<boats.length; i++){
-		        	var boat = boats[i];
-					var boatObj = {};
+                for (var i = 0; i<boats.length; i++){
+                    var boat = boats[i];
+                    var boatObj = {};
 
-				  	boatObj.name = boat.name;
-				  	boatObj.port = boat.port;
-				  	boatObj.active = boat.active;
+                      boatObj.name = boat.name;
+                      boatObj.port = boat.port;
+                      boatObj.active = boat.active;
 
-					getLastPullTime(boat, function(result){
-						boatObj.lastUpdated = result;
-						getUptime(boat, function(result){
-							boatObj.uptime = result;
-							//Add the boat to the fleet
-							fleetOfBoats.push(boatObj);
+                    async.series([
+                        getLastPullTime(boat, function(result){
+                            boatObj.lastUpdated = result;
+                        }),
+                        getUptime(boat, function(result){
+                            boatObj.uptime = result;
+                            fleetOfBoats.push(boatObj);
+                        })
+                    ]);
 
-							console.log(fleetOfBoats);
+                    console.log(fleetOfBoats);
 
-							if(i == boats.length-1) {
-								return res.json(fleetOfBoats);
-							}
-						});
-					});
-			  	}
-        	}
- 		});
-	}
+                    return res.json(fleetOfBoats);
+                }
+            }
+         });
+    }
 };
