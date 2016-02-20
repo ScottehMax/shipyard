e9c660878e558878c4b44f5f0eb785aaced4a713
Nathan Douglas <nd9600@gmail.com>
2016-02-20 14:54:57 +0000

    Started user model

--- syWeb/api/models/User.js
+++ syWeb/api/models/User.js
@@ -1,14 +1,19 @@
 /**
 * User.js
 *
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @description :: This model represents the User entity
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
 module.exports = {
 
-  attributes: {
+    attributes: {
+        name: {
+            type: 'string'
+        },
 
-  }
+        email: {
+            type: 'string'
+        }
+    }
 };
-
