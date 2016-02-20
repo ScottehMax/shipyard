8a77c28d3fce39827f5144916883a66e04bfe55c
Jordan Duncan <jordan@dogfi.sh>
2016-02-20 14:16:58 +0000

    models + controllers

---
+++ syWeb/api/controllers/BoatController.js
@@ -0,0 +1,11 @@
+/**
+ * BoatController
+ *
+ * @description :: Server-side logic for managing Boats
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ syWeb/api/controllers/HarbourController.js
@@ -0,0 +1,11 @@
+/**
+ * HarbourController
+ *
+ * @description :: Server-side logic for managing Harbours
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
