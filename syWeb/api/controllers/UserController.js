ad79b73c06bc0384204436673b471d866c76df0e
Nathan Douglas <nd9600@gmail.com>
2016-02-20 15:32:17 +0000

    Started authentication

---
+++ syWeb/api/controllers/UserController.js
@@ -0,0 +1,10 @@
+/**
+ * BoatController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
--- syWeb/api/models/User.js
+++ syWeb/api/models/User.js
@@ -8,12 +8,44 @@
 module.exports = {
 
     attributes: {
+        username: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+
+        password: {
+            type: 'string',
+            required: true
+        },
+
+        toJSON: function() {
+            var obj = this.toObject();
+            delete obj.password;
+            return obj;
+        },
+
         name: {
-            type: 'string'
+            type: 'string',
+            required: true
         },
 
         email: {
             type: 'string'
         }
+    },
+
+    beforeCreate: function(user, cb) {
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) {
+                    console.log(err);
+                    cb(err);
+                } else{
+                    user.password = hash;
+                    cb(null, user);
+                }
+            });
+        });
     }
 };
