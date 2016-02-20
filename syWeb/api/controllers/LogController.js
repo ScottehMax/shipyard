c536523fd36cc42db05455fc6b41c68f3657d21a
Jordan Duncan <jordan@dogfi.sh>
2016-02-20 17:19:31 +0000

    log controller

---
+++ syWeb/api/controllers/LogController.js
@@ -0,0 +1,10 @@
+/**
+ * LogController
+ *
+ * @description :: Server-side logic for managing Logs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
