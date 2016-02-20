b960db2ababb3fe7823a01397a4757c983535bed
Jordan Duncan <jordan@dogfi.sh>
2016-02-20 20:46:17 +0000

    test boat

--- syWeb/api/controllers/HarbourController.js
+++ syWeb/api/controllers/HarbourController.js
@@ -6,6 +6,29 @@
  */
 
 module.exports = {
-	
-};
+  index: function(req, res) {
+    var fleetOfBoats = [];
 
+    Boat.find().exec(function(err, boats) {
+      if (err) {
+        return res.json({
+          error: err
+        });
+      }
+
+      if (boats === undefined) {
+        return res.notFound();
+      } else {
+
+        for (var i = 0; i < boats.length; i++) {
+          var boat = boats[i];
+
+          boat.uptime = Math.floor((Date.now() - boat.lastUpdated) / 1000);
+
+          fleetOfBoats.push(boat);
+        }
+        return res.view('homepage', {boats: fleetOfBoats});
+      }
+    });
+  }
+};
