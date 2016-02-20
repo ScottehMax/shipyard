451384965fadec9221df4276aa27974aa5c74f24
Nathan Douglas <nd9600@gmail.com>
2016-02-20 16:58:22 +0000

    Started boatController

--- syWeb/api/controllers/BoatController.js
+++ syWeb/api/controllers/BoatController.js
@@ -6,6 +6,30 @@
  */
 
 module.exports = {
-	
-};
 
+	get: function (req, res) {
+		var id = req.param('id');
+		Boat.findOne({id:id})
+        .exec(function (err,boat) {
+          if(err){
+            return res.json({
+              error:err
+            });
+          }
+          if(boat === undefined) {
+            return res.notFound();
+          }
+          else {
+			  var boatObj = {};
+			  boatObj.name = boat.name;
+			  boatObj.port = boat.port;
+			  boatObj.active = boat.active;
+			  return res.json(boatObj);
+		  }
+        });
+ 	}
+
+
+
+
+};
--- syWeb/api/models/Log.js
+++ syWeb/api/models/Log.js
@@ -21,5 +21,9 @@ module.exports = {
             type: 'datetime'
         },
 
+        boat: {
+            model: 'boat'
+        }
+
     }
 };
