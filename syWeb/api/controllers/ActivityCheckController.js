3708486b00ab55d7bc07aeaf7f5f62ca2179adb0
Nathan Douglas <nd9600@gmail.com>
2016-02-21 09:39:54 +0000

    Changes active status of boats

--- syWeb/api/controllers/ActivityCheckController.js
+++ syWeb/api/controllers/ActivityCheckController.js
@@ -7,10 +7,7 @@
 
 var http = require('http');
 var util  = require('util');
-const spawnSync = require('child_process').spawnSync;
-var spawn = require('child_process').spawn;
 var execSync = require('child_process').execSync;
-var util = require('util');
 
 function getAllBoats(callback) {
 
@@ -39,22 +36,44 @@ function getAllBoats(callback) {
 
 module.exports = {
     checkActivityStatus: function(req,res){
+
+        // boatModelArray = [];
+        //
+        // Boat.find().exec(function(err, boatModels) {
+        //   if (err) {
+        //     return res.json({
+        //       error: err
+        //     });
+        //   }
+        //
+        //   if (boatModels === undefined) {
+        //     return res.notFound();
+        //   } else {
+        //
+        //     for (var i = 0; i < boatModels.length; i++) {
+        //       var boatModel  = boatModels[i];
+        //
+        //       boatModelArray.push(boatModel);
+        //     }
+        //   }
+        // });
+
         getAllBoats(function(boats) {
             console.log("In end of callback");
             console.log("boats: ", boats);
 
             var isActiveArray = [];
 
-            console.log("boats: ", boats);
-            console.log("boats.length: ", boats.length);
+            //console.log("boats: ", boats);
+            //console.log("boats.length: ", boats.length);
 
             for (var i = 0; i < boats.length; i++) {
               var boat = boats[i];
 
-              console.log("");
-              console.log("boat.id: ", boat.id);
-              console.log("boat.name: ", boat.name);
-              console.log("boat.mainAppFile: ", boat.mainAppFile);
+              //console.log("");
+              //console.log("boat.id: ", boat.id);
+              //console.log("boat.name: ", boat.name);
+              //console.log("boat.mainAppFile: ", boat.mainAppFile);
 
               var longString = "ps aux | grep \"/apps/" + boat.id+ "/" + boat.mainAppFile + "\" | wc -l";
 
@@ -63,24 +82,52 @@ module.exports = {
               var numberOfProcesses = command - 2;
               var isActive = (numberOfProcesses > 0);
 
-              isActiveArray.push([boat.id ,isActive]);
+              var boatModel = User.findOne(boat.id).done(function(error, boatModel) {
+                  if(error) {
+                     // do something with the error.
+                  }
 
-              /*
+                  //Save it to the object.
+                  boatModel.active = isActive;
+
+                  console.log("Updated boat #", boatModel.id, "'s activity status'");
+
+                  boatModel.save(function(error) {
+                      if(error) {
+                          // do something with the error.
+                      } else {
+                          // value saved!
+                          req.send(boatModel);
+                      }
+                  });
+              });
+
+            //   Boat.update({id:boat.id}).exec(function afterwards(err, updated){
+              //
+            //       if (err) {
+            //         // handle error here- e.g. `res.serverError(err);`
+            //         return;
+            //       }
+              //
+            //       console.log('Updated user to have name ' + updated[0].name);
+            //   });
+
+              isActiveArray.push([boat.id, isActive]);
+
               if (numberOfProcesses > 0){
                   console.log("App #",boat.id, " is running");
               } else {
-                  console.log("App #",boat.id, " isn't running")
+                  console.log("App #",boat.id, " isn't running");
               }
 
-              //console.log("Command: ", command);
-              //console.log("numberOfProcesses: ", numberOfProcesses);
-              //console.log("isActive: ", isActive);
+              /*
+              console.log("Command: ", command);
+              console.log("numberOfProcesses: ", numberOfProcesses);
+              console.log("isActive: ", isActive);
               */
 
             }
             return res.json(isActiveArray);
         });
-
-        console.log("After callback");
     }
 };
