4e8a50835f2d11d398bc106608f7ce856ef0b784
Jordan Duncan <jordan@dogfi.sh>
2016-02-21 11:28:29 +0000

    pushbullet

--- syWeb/api/controllers/WebhookController.js
+++ syWeb/api/controllers/WebhookController.js
@@ -35,4 +35,30 @@ module.exports = {
 		})
 		return res.send(fug);
 	}
+
+	stop: function(req, res) {
+		var fug = 'aaa';
+		var id = req.param('id');
+		Boat.findOne({id: id}).exec(function(err, boat) {
+			if (err) {
+				console.log('err');
+				return res.json({error: err});
+			}
+			if (!boat) {
+				console.log('no boat :(');
+			} else {
+				// everything worked, our ship has come in!
+				console.log('running git pull...');
+				execSync('git pull', {'cwd': './apps/' + id});
+				// fucking race conditions...
+				console.log('running forever app.js...');
+				try {
+					execSync('forever stop app.js', {'cwd': './apps/' + id});
+				} catch (e) {
+					console.log('app is not running, or something else went wrong. blame jordan');
+				}
+			}
+		})
+		return res.send(fug);
+	}
 };
--- syWeb/api/models/Log.js
+++ syWeb/api/models/Log.js
@@ -21,5 +21,43 @@ module.exports = {
             type: 'integer'
         }
 
-    }
+    },
+
+    afterCreate: function (entry, cb) {
+      var request = require('request');
+
+      // Set the headers
+      var headers = {
+          'Content-Type':     'application/x-www-form-urlencoded',
+          'Access-Token': 'o.PEpRgkpb7lcqbrQPOM2GStlLdyP9IBjq'
+      }
+
+      // Configure the request
+      var options = {
+          url: 'https://api.pushbullet.com/v2/pushes',
+          method: 'POST',
+          headers: headers,
+          form: {
+            'type': 'note',
+            'title' : 'Shipyard | ' + entry.name,
+            'body' : entry.message,
+
+          }
+      }
+
+      // Start the request
+      request(options, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              // Print out the response body
+              console.log(body)
+          }
+      })
+      // Encrypt password
+      bcrypt.hash(values.password, 10, function(err, hash) {
+        if(err) return cb(err);
+        values.password = hash;
+        //calling cb() with an argument returns an error. Useful for canceling the entire operation if some criteria fails.
+        cb();
+      });
+  }
 };
